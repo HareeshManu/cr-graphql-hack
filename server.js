8aa24c3b15937ae4007f9cece1becbc6b2e4b19c
hgopi <v-hgopi@carrentals.com>
2020-07-07 20:25:20 +0530

    remove redis

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-import Redis from "ioredis";
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
 const PORT = 4000;
 
 const app = express();
-const redis = new Redis();
 
 const server = new ApolloServer({ typeDefs, resolvers, context: { redis } });
 server.applyMiddleware({ app });
