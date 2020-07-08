0fd567c2c6aa4df666030d85ca51ac46fe3b972a
hgopi <v-hgopi@carrentals.com>
2020-07-08 11:47:38 +0530

    path changes

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const PORT = 4000;
 const app = express();
 
 const server = new ApolloServer({ typeDefs, resolvers});
-server.applyMiddleware({ app });
+server.applyMiddleware({ app , path: '/'});
 
-app.listen({ port: PORT }, () =>
+app.listen({ port: PORT}, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 )
